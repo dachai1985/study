7a8a28db2e660dfc83d185050315098610bd464d
dachai1985 <dachai1985@sohu.com>
2025-01-09 14:24:46 +0800

    run.py modify

--- Appium/Demo/Tests/RunAllTest.py
+++
@@ -1,8 +0,0 @@
-import unittest
-
-test_dir = './Tests' #用力目录
-discover = unittest.defaultTestLoader.discover(test_dir, pattern='test*.py')
-
-if __name__ == '__main__':
-    runner = unittest.TextTestRunner()
-    runner.run(discover)
